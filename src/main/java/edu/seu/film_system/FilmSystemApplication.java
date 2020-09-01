e247cd70ad3dc516bde01fd9fb4dfdf8dfeaf484
许立崭 <Xulizhan0302@outlook.com>
2020-09-01 18:07:52 +0800

    Initial commit

---
+++ src/main/java/edu/seu/film_system/FilmSystemApplication.java
@@ -0,0 +1,13 @@
+package edu.seu.film_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/seu/film_system/FilmSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.seu.film_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
