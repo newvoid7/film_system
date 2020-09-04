c856115ef749159654f4f72f67fedc8d894c78c5
许立崭 <Xulizhan0302@outlook.com>
2020-09-05 07:59:22 +0800

    UserController 上传头像接口接收用户ID表单

--- src/main/java/edu/seu/film_system/controller/UserController.java
+++ src/main/java/edu/seu/film_system/controller/UserController.java
@@ -135,14 +135,22 @@ public class UserController {
     // 更新用户昵称
     // TODO 用 xml 中的函数？还是直接传入 json
 
+    // 上传头像，以用户 ID 命名，存在服务器端指定文件夹下
+    // 默认覆盖相同名称的文件（包括后缀）
+    // TODO 怎么样保持用户只有一个头像资源，即使后缀不同？
     // http://127.0.0.1:8256/film_system/user/uploadAvatar
     // 返回头像的静态资源网址
     @RequestMapping(value="/uploadAvatar")
     @ResponseBody
-    public String uploadImg(@RequestParam("img") MultipartFile img) {
-        String contentType = img.getContentType();    // 获取文件的类型
+    public String uploadImg(@RequestParam("img") MultipartFile img, @RequestParam("userId")int userId) {
+        // String contentType = img.getContentType();    // 获取文件的类型
         // System.out.println("文件类型为：" + contentType);
+        // System.out.println(userId);
         String originalFilename = img.getOriginalFilename();     // 获取文件的原始名称
+        String suffix = "";
+        if (originalFilename.lastIndexOf(".") != -1) {
+            suffix = originalFilename.substring(originalFilename.lastIndexOf("."));  // 文件后缀
+        }
         String pathname = "D:/film/user";
         // 判断文件是否为空
         if (!img.isEmpty()) {
@@ -153,7 +161,7 @@ public class UserController {
             }
             try {
                 // 开始保存图片
-                FileOutputStream outputStream = new FileOutputStream(pathname + '/' + originalFilename);
+                FileOutputStream outputStream = new FileOutputStream(pathname + '/' + userId + suffix);
                 outputStream.write(img.getBytes());
                 outputStream.flush();
                 outputStream.close();
@@ -161,7 +169,7 @@ public class UserController {
                 e.printStackTrace();
             }
         }
-        return "http://127.0.0.1:8256/film_system/resource/user/"+originalFilename;
+        return "http://127.0.0.1:8256/film_system/resource/user/" + userId + suffix;
     }
 }
 
